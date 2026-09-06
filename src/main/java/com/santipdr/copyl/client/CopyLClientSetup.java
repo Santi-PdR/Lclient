3dc7568f010e9c06cd9682526418f8dbcc3cccb2
Santi.pdr <santiagopereiraxd029@gmail.com>
2026-09-06 02:13:02 -0300

    feat: add Mods config screen entry

---
+++ src/main/java/com/santipdr/copyl/client/CopyLClientSetup.java
@@ -0,0 +1,19 @@
+package com.santipdr.copyl.client;
+
+import com.santipdr.copyl.client.screen.MessageEditorScreen;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.ModLoadingContext;
+
+public final class CopyLClientSetup {
+    private CopyLClientSetup() {
+    }
+
+    public static void registerConfigScreen() {
+        ModLoadingContext.get().registerExtensionPoint(
+                ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory(
+                        (minecraft, parent) -> new MessageEditorScreen(parent)
+                )
+        );
+    }
+}
