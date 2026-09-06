36ba4b9d4b69c07ee19226c9564592754235a7a2
Santi.pdr <santiagopereiraxd029@gmail.com>
2026-09-06 18:47:00 -0300

    chore: rename Windows wrapper bootstrap message

--- gradlew.bat
+++ gradlew.bat
@@ -5,7 +5,7 @@ set "WRAPPER_JAR=%APP_HOME%gradle\wrapper\gradle-wrapper.jar"
 
 if not exist "%WRAPPER_JAR%" (
     if not exist "%APP_HOME%gradle\wrapper" mkdir "%APP_HOME%gradle\wrapper"
-    echo CopyL: downloading Gradle wrapper bootstrap...
+    echo Lclient: downloading Gradle wrapper bootstrap...
     powershell -NoProfile -ExecutionPolicy Bypass -Command "$ProgressPreference='SilentlyContinue'; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/gradle/gradle/v8.1.1/gradle/wrapper/gradle-wrapper.jar' -OutFile '%WRAPPER_JAR%'"
     if errorlevel 1 exit /b 1
 )
